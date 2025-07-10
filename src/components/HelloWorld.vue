3beeb2afaab44db259a26a874a7edfe1482086d6
ldegrande <lucas.degrande@althaia.com.br>
2025-07-10 14:08:36 -0300

    feat: adding title for images and height display

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,175 +1,87 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
-        </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
-
-<!--
-  Carousel Example
-<template>
   <v-app>
-    <v-container>
-      <v-carousel show-arrows="hover">
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          cover
-        ></v-carousel-item>
+    <v-carousel show-arrows="hover" :height="$vuetify.display.height - 64">
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+      >
+        <div class="d-flex align-start justify-start fill-height">
+          <h2 class="text-white text-h5 ma-4">Título da Imagem 1</h2>
+        </div>
+      </v-carousel-item>
 
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-          cover
-        ></v-carousel-item>
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+        cover
+      >
+        <div class="d-flex align-start justify-start fill-height">
+          <h2 class="text-white text-h5 ma-4">Outro Título Aqui</h2>
+        </div>
+      </v-carousel-item>
 
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          cover
-        ></v-carousel-item>
-      </v-carousel>
-    </v-container>
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        cover
+      >
+        <div class="d-flex align-start justify-start fill-height">
+          <h2 class="text-white text-h5 ma-4">Mais um Título</h2>
+        </div>
+      </v-carousel-item>
+    </v-carousel>
   </v-app>
 </template>
--->
 
 <script>
-
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
   data: () => ({
     ecosystem: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader/tree/next",
       },
       {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify/tree/next",
       },
       {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
       },
     ],
     importantLinks: [
       {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
       },
       {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
       },
       {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
       },
       {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
+        text: "Articles",
+        href: "https://medium.com/vuetify",
       },
     ],
     whatsNext: [
       {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
+        text: "Explore components",
+        href: "https://vuetifyjs.com",
       },
       {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
+        text: "Roadmap",
+        href: "https://vuetifyjs.com/introduction/roadmap/",
       },
       {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
       },
     ],
   }),
-}
+};
 </script>
