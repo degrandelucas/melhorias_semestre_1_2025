50e8d703a2b071625a7fd4048b4e404599c5d2df
degrandelucas <lucasdegrande15@gmail.com>
2025-07-12 18:22:37 -0300

    feat: inclusao capa da apresentacao e alteracao no tamanho das imagens da caixa

--- src/components/MelhoriasIgreja.vue
+++ src/components/MelhoriasIgreja.vue
@@ -20,6 +20,7 @@ export default {
   name: "MelhoriasIgreja",
   data: () => ({
     carouselItems: [
+      { src: require("../assets/assets/capa.jpeg"), title: "" },
       { src: require("../assets/assets/lousa_escola_biblica.jpeg"), title: "Lousa Escola Bíblica Dominical" },
       { src: require("../assets/assets/sala_juniores.jpeg"), title: "Sala Juniores (Casinha)" },
       { src: require("../assets/assets/sala_criancas.jpeg"), title: "Sala Crianças (Casinha)" },
@@ -27,12 +28,14 @@ export default {
       { src: require("../assets/assets/dispensa_alimentacao.jpeg"), title: "Alimentação, Itens de Limpeza, Estrutura para Armazenamento" },
       { src: require("../assets/assets/forro_armacao_1.jpeg"), title: "Armação nova para o Templo" },
       { src: require("../assets/assets/forro_armacao_2.jpeg"), title: "Armação nova para o Templo" },
-      { src: require("../assets/assets/forro_quase_finalizado.jpeg"), title: "Forro quase sendo Finalizado" },
+      { src: require("../assets/assets/forro_quase_finalizado.jpeg"), title: "Forro quase sendo finalizado" },
       { src: require("../assets/assets/forro_finalizado.jpeg"), title: "Novo Forro Igreja" },
       { src: require("../assets/assets/forro_novas_lampadas.jpeg"), title: "Novas Lâmpadas, Melhor Iluminação" },
       { src: require("../assets/assets/ventiladores.jpeg"), title: "2 Novos Ventiladores" },
-      { src: require("../assets/assets/caixa_som_altar.jpeg"), title: "Nova Caixa de Som Retorno Altar" },
-      { src: require("../assets/assets/caixa_som_lateral.jpeg"), title: "Nova Caixa de Som (Lateral do Tempo) e Pintura Paredes do Altar" },
+      { src: require("../assets/assets/microfone.jpeg"), title: "Melhor Microfone" },
+      { src: require("../assets/assets/conjunto_microfones.jpeg"), title: "Conjunto de Microfones Novos" },
+      { src: require("../assets/assets/caixa_som_altar_2.jpeg"), title: "Nova Caixa de Som Retorno Altar" },
+      { src: require("../assets/assets/caixa_som_lateral_2.jpeg"), title: "Nova Caixa de Som (Lateral do Tempo) e Pintura Paredes do Altar" },
     ],
   }),
 };
