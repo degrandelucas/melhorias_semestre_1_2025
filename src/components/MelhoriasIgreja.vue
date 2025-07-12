74ece2ce78362cbbba88092790a76bbb4426c25c
degrandelucas <lucasdegrande15@gmail.com>
2025-07-12 17:16:30 -0300

    refactor: Otimizando codigo utilizando v-for para iterar sobre as imagens e titulos de cada uma

--- src/components/MelhoriasIgreja.vue
+++ src/components/MelhoriasIgreja.vue
@@ -2,109 +2,13 @@
   <v-app>
     <v-carousel show-arrows="hover" :height="$vuetify.display.height">
       <v-carousel-item
-        src="../assets/assets/lousa_escola_biblica.jpeg"
+        v-for="(item, i) in carouselItems"
+        :key="i"
+        :src="item.src"
         cover
       >
         <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Lousa Escola Bíblica Dominical</h2>
-        </div>
-      </v-carousel-item>
-
-      <v-carousel-item
-        src="../assets/assets/sala_juniores.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Sala Juniores (Casinha)</h2>
-        </div>
-      </v-carousel-item>
-
-      <v-carousel-item
-        src="../assets/assets/sala_criancas.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Sala Crianças (Casinha)</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/cozinha_casinha.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Cozinha (Casinha)</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/dispensa_alimentacao.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Alimentação, Itens de Limpeza, Estrutura para Armazenamento</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/forro_armacao_1.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Armação nova para o Templo</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/forro_armacao_2.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Armação nova para o Templo</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/forro_quase_finalizado.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Forro quase sendo finalizado</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/forro_finalizado.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Novo Forro Igreja</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/forro_novas_lampadas.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Novas Lâmpadas, Melhor Iluminação</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/ventiladores.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">2 Novos Ventiladores</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/caixa_som_altar.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Nova Caixa de Som Retorno Altar</h2>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item
-        src="../assets/assets/caixa_som_lateral.jpeg"
-        cover
-      >
-        <div class="d-flex align-start justify-start fill-height">
-          <h2 class="text-white text-h5 ma-4">Nova Caixa de Som (Lateral do Tempo) e Pintura Paredes do Altar</h2>
+          <h2 class="text-white text-h4 ma-4">{{ item.title }}</h2>
         </div>
       </v-carousel-item>
     </v-carousel>
@@ -114,9 +18,22 @@
 <script>
 export default {
   name: "MelhoriasIgreja",
-
   data: () => ({
-    
+    carouselItems: [
+      { src: require("../assets/assets/lousa_escola_biblica.jpeg"), title: "Lousa Escola Bíblica Dominical" },
+      { src: require("../assets/assets/sala_juniores.jpeg"), title: "Sala Juniores (Casinha)" },
+      { src: require("../assets/assets/sala_criancas.jpeg"), title: "Sala Crianças (Casinha)" },
+      { src: require("../assets/assets/cozinha_casinha.jpeg"), title: "Cozinha (Casinha)" },
+      { src: require("../assets/assets/dispensa_alimentacao.jpeg"), title: "Alimentação, Itens de Limpeza, Estrutura para Armazenamento" },
+      { src: require("../assets/assets/forro_armacao_1.jpeg"), title: "Armação nova para o Templo" },
+      { src: require("../assets/assets/forro_armacao_2.jpeg"), title: "Armação nova para o Templo" },
+      { src: require("../assets/assets/forro_quase_finalizado.jpeg"), title: "Forro quase sendo Finalizado" },
+      { src: require("../assets/assets/forro_finalizado.jpeg"), title: "Novo Forro Igreja" },
+      { src: require("../assets/assets/forro_novas_lampadas.jpeg"), title: "Novas Lâmpadas, Melhor Iluminação" },
+      { src: require("../assets/assets/ventiladores.jpeg"), title: "2 Novos Ventiladores" },
+      { src: require("../assets/assets/caixa_som_altar.jpeg"), title: "Nova Caixa de Som Retorno Altar" },
+      { src: require("../assets/assets/caixa_som_lateral.jpeg"), title: "Nova Caixa de Som (Lateral do Tempo) e Pintura Paredes do Altar" },
+    ],
   }),
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/melhorias_semestre_1_2025/'
+    : '/',
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
